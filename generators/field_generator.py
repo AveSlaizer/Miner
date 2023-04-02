3ce5fea153ec9dec5f21390ce157fc4ba78708cc
Sushkov AS <aveslaizer@gmail.com>
2023-04-02 12:11:08 +0300

    Реализовал класс FieldGenerator

--- generators/field_generator.py
+++ generators/field_generator.py
@@ -1,6 +1,18 @@
 from random import randint
 from typing import List
+from .field_cell import FieldCell
 
 
 class FieldGenerator:
-    pass
+
+    def __init__(self, rows: int = None, columns: int = None, mines_qty: int = None):
+        self.__rows = rows
+        self.__columns = columns
+        self.__mines_qty = mines_qty
+
+    def __str__(self):
+        return f"Поле {self.__rows} рядов на {self.__columns} столбцов, {self.__mines_qty} мин."
+
+
+    def __get_mine_coordinates(self):
+        pass
