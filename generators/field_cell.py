ac0e6eeedc3f39d479af630ed98a7f273dbdc0c5
Sushkov AS <aveslaizer@gmail.com>
2023-04-02 11:39:40 +0300

    Написал методы, геттеры, сеттеры классу FieldCell

--- generators/field_cell.py
+++ generators/field_cell.py
@@ -1,6 +1,6 @@
 class FieldCell:
 
-    def __init__(self, value: int = None, row: int = None, column: int = None, status: str = None):
+    def __init__(self, value: int = 0, row: int = None, column: int = None, status: bool = True):
         self.__value = value
         self.__row = row
         self.__column = column
@@ -12,4 +12,45 @@ class FieldCell:
     def info(self):
         print(f"Значение: {self.__value}\n"
               f"Адресс: строка {self.__row} столбец {self.__column}\n"
-              f"Статус: {self.__status}")
+              f"Статус: {FieldCell.status}")
+
+    def set_mine(self):
+        if self.__value == 0:
+            self.__value = -1
+        raise Exception("Для установки мины значение ячейки должно быть равным 0")
+
+    def increase_value(self):
+        if self.__value >= 0:
+            self.__value += 1
+        raise Exception("В ячейке установлена мина")
+
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def row(self):
+        return self.row
+
+    @row.setter
+    def row(self, row: int):
+        self.__row = row
+
+    @property
+    def column(self):
+        return self.__column
+
+    @column.setter
+    def column(self, column: int):
+        self.__column = column
+
+    @property
+    def status(self):
+        if self.__status:
+            return "Закр"
+        return "Откр"
+
+
+
+
+
