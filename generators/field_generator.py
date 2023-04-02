39df936950900d98cf38348a8282028abb44244e
Sushkov AS <aveslaizer@gmail.com>
2023-04-02 13:04:25 +0300

    написал метод get_mine_coordinates

--- generators/field_generator.py
+++ generators/field_generator.py
@@ -25,8 +25,8 @@ class FieldGenerator:
     def mines_qty(self):
         return self.__mines_qty
 
-    @staticmethod
-    def get_empty_pole(columns: int, rows: int) -> List[List[FieldCell]]:
+
+    def get_empty_pole(self) -> List[List[FieldCell]]:
         """
         Возвращает двумерный список widht x height заполненный нулями (пустое поле)
 
@@ -35,4 +35,27 @@ class FieldGenerator:
         :return:
                 List[List[int]]: Двумерный список из объектов класса FieldCell
         """
-        return [[FieldCell(row = j, column = i) for i in range(columns)] for j in range(rows)]
+        return [[FieldCell(row = j, column = i) for i in range(self.__columns)] for j in range(self.rows)]
+
+
+    def get_mine_coordinates(self) -> List[List[int]]:
+        """
+        Возвращает список с уникальными координтами мин в виде [x, y]
+
+        :param quantity (int): Количество мин
+        :param columns (int): Ширина
+        :param rows (int): Высота
+        :return:
+                List[List[int]]: Список уникальных списоков с координатами мин
+        """
+        mine_list = []
+        temp_count = 0
+        while temp_count < self.__mines_qty:
+            mine_col = randint(0, self.__columns - 1)
+            mine_row = randint(0, self.__rows - 1)
+            mine_coordinate = [mine_col, mine_row]
+            if mine_coordinate not in mine_list:
+                mine_list.append(mine_coordinate)
+                temp_count += 1
+        return mine_list
+
